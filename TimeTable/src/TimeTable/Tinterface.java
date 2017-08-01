c50775510721f72401df81d35f0b7d772e6db48e
aimee <aimee@156.59.107.149>
2017-08-02 11:39:28 +1200

    TimeTable interface

---
+++ TimeTable/src/TimeTable/Tinterface.java
@@ -0,0 +1,54 @@
+package TimeTable;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+
+public class Tinterface {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tinterface window = new Tinterface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Tinterface() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.WEST);
+		
+		textField = new JTextField();
+		frame.getContentPane().add(textField, BorderLayout.CENTER);
+		textField.setColumns(10);
+	}
+
+}
