60fa143967bc824c740318addf5670544879f79b
Bruce Li <zilong.li@it.weltec.ac.nz>
2017-08-07 14:17:25 +1200

    new interface

---
+++ TimeTable/src/TimeTable/SelectCourse.java
@@ -0,0 +1,36 @@
+package TimeTable;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+public class SelectCourse extends Tinterface{
+	
+	public ArrayList<Map<String, Object>> matchLevel(String level) {
+		
+    ArrayList courseList = new ArrayList();	
+    
+	DBConnection DBC = new DBConnection();
+	ResultSet rs = DBC.queryExecute("select NAME from course where LEVEL = ? ", level);
+	
+		try {
+			
+		while (rs.next()) {//数据集不为空
+			courseList.add(rs.getObject(1));		
+			}
+		
+		System.out.println(courseList);
+		
+		}
+		catch (Exception e) {  
+            // TODO: handle exception  
+            e.printStackTrace();  
+      }  
+		return courseList;
+		
+	}
+	
+	
+}
--- TimeTable/src/TimeTable/Tinterface.java
+++ TimeTable/src/TimeTable/Tinterface.java
@@ -6,15 +6,38 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.List;
+
+import javax.swing.BoxLayout;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class Tinterface {
 
 	private JFrame frame;
-	private JTextField textField;
-
-	/**
-	 * Launch the application.
-	 */
+	String selectLevel;
+     ArrayList list = new ArrayList();
+    
+    String[] cString  = { "please select level"};
+     JComboBox cBoxCourse = new JComboBox(cString);
+ 	 
+ 	
+	public void course() {
+		cBoxCourse.setToolTipText("123");
+		cBoxCourse.setBounds(236, 7, 94, 24);
+		frame.getContentPane().add(cBoxCourse);
+    
+	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -26,29 +49,87 @@ public class Tinterface {
 				}
 			}
 		});
+		
+		
+		
 	}
 
 	/**
 	 * Create the application.
 	 */
 	public Tinterface() {
+		
 		initialize();
+		course();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 496, 376);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		frame.getContentPane().add(lblNewLabel, BorderLayout.WEST);//to set up layout
+		JLabel levelLable = new JLabel("Level");
+		levelLable.setBounds(12, 12, 60, 15);
+		levelLable.setVerticalAlignment(SwingConstants.TOP);
+		frame.getContentPane().add(levelLable);
 		
-		textField = new JTextField();
-		frame.getContentPane().add(textField, BorderLayout.CENTER);
-		textField.setColumns(10);
-	}
+		JLabel lblCourse = new JLabel("Course");
+		lblCourse.setBounds(182, 12, 60, 15);
+		frame.getContentPane().add(lblCourse);
+		
+		
+		String[] levelNum = {"5", "6", "7"};
+		JComboBox cBoxLevel = new JComboBox(levelNum);
+		cBoxLevel.addItemListener(new ItemListener() {
+		 
+			public void itemStateChanged(ItemEvent e) {
+				 if(e.getStateChange() == ItemEvent.SELECTED) {
+			 
+				SelectCourse SC = new SelectCourse();
+			    selectLevel = (String) cBoxLevel.getSelectedItem();
+			    list.clear();
+			    course();
+			    list.addAll(SC.matchLevel(selectLevel));
+			  
+			    cBoxCourse.addItem(list);
+		
+				 }
+			}
+		});
+	
 
+		
+		cBoxLevel.setBounds(59, 7, 94, 24);
+		frame.getContentPane().add(cBoxLevel);
+		
+		
+	//JComboBox cBoxCourse = new JComboBox();
+		/*cBoxCourse.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if(e.getStateChange() == ItemEvent.SELECTED)
+				cBoxCourse.addItem(list);
+				//System.out.print(list);
+			}
+			
+		});*/
+	/*	cBoxCourse.setToolTipText("");
+		cBoxCourse.setBounds(236, 7, 94, 24);
+		frame.getContentPane().add(cBoxCourse);
+		cBoxCourse.addItem("please select level");*/
+		
+		JLabel lblStream = new JLabel("Stream");
+		lblStream.setBounds(346, 12, 60, 15);
+		frame.getContentPane().add(lblStream);
+		
+		JComboBox cBoxStream = new JComboBox();
+		cBoxStream.setBounds(393, 7, 91, 24);
+		frame.getContentPane().add(cBoxStream);
+	}
 }
