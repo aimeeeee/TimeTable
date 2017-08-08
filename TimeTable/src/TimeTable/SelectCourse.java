e6030af052d4656d2b277e09af211d6fd4cedc84
Bruce Li <zilong.li@it.weltec.ac.nz>
2017-08-08 21:09:27 +1200

    Add SelectCourse

---
+++ TimeTable/src/TimeTable/SelectCourse.java
@@ -0,0 +1,37 @@
+package TimeTable;
+
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+public class SelectCourse extends Tinterface{
+	
+	public ArrayList<String> matchLevel(String level) {
+		
+    ArrayList<String> courseList = new ArrayList();	
+    
+	DBConnection DBC = new DBConnection();
+	ResultSet rs = DBC.queryExecute("select NAME from course where LEVEL = ? ", level);
+	
+		try {
+			
+		while (rs.next()) {//数据集不为空
+			courseList.add((String) rs.getObject(1));		
+			}
+		
+		System.out.println(courseList);
+		
+		}
+		catch (Exception e) {  
+            e.printStackTrace();  
+      }  
+//		String[] stringArray = courseList.toArray(new String[0]);
+//		return stringArray;
+		return courseList;
+		
+	}
+	
+	
+}
